c915fcb6920449facc9618ab14cee9159b5cef81
Carlos <krloslao90@gmail.com>
2021-08-02 18:30:12 -0400

    wrapping up

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,41 +5,132 @@ import Item from "./Item";
 import "../styles/ItemList.css";
 
 function ItemList() {
-  const [ItemListData, setItemListData] = useState(null);
+  const [Search, setSearch] = useState("");
+  const [SortBy, setSortBy] = useState("name");
+  const [ScrollPage, setScrollPage] = useState();
+  const [Direction, setDirection] = useState("asc");
+  const [ItemListData, setItemListData] = useState([]);
+  const [PaginationBtn, setPaginationBtn] = useState();
 
   useEffect(() => {
-    fetch("https://rma.swissgear.com/api/interview-endpoint")
+    setScrollPage(1);
+    setPaginationBtn(true);
+    fetch(
+      "https://rma.swissgear.com/api/interview-endpoint?q=" +
+        Search +
+        "&sortBy=" +
+        SortBy +
+        "&direction=" +
+        Direction +
+        "&page=" +
+        ScrollPage
+    )
       .then((res) => res.json())
       .then(
         (result) => {
           setItemListData(result.data);
-          console.log(result.data);
+          if (result.data.length < 16) {
+            setPaginationBtn(false);
+          }
         },
         (error) => {
           console.log(error);
         }
       );
-  }, []);
+  }, [Search, SortBy, Direction]);
+
+  useEffect(() => {
+    if (ScrollPage != 1) {
+      fetch(
+        "https://rma.swissgear.com/api/interview-endpoint?q=" +
+          Search +
+          "&sortBy=" +
+          SortBy +
+          "&direction=" +
+          Direction +
+          "&page=" +
+          ScrollPage
+      )
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            setItemListData([...ItemListData, ...result.data]);
+            console.log(result.data);
+            if (result.data.length < 16) {
+              setPaginationBtn(false);
+            }
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    }
+  }, [ScrollPage]);
 
   if (!ItemListData) {
     return <h1>Loading...</h1>;
   }
 
   return (
-    <ul className="ItemList">
-      {ItemListData.map((item) => (
-        <li key={item.sku}>
-          <Item
-            url={item.url}
-            alt={item.name}
-            name={item.name}
-            price={item.price}
-            src={item.base_image}
-            hover_image={item.hover_image}
-          />
-        </li>
-      ))}
-    </ul>
+    <>
+      <div className="ItemWrapper">
+        <input
+          className="SearchBar"
+          type="text"
+          placeholder="Search"
+          value={Search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <form className="SortBy">
+          <label>
+            Sort By:
+            <select value={SortBy} onChange={(e) => setSortBy(e.target.value)}>
+              <option value="name">Name</option>
+              <option value="price">Price</option>
+            </select>
+          </label>
+        </form>
+        <form className="Direction">
+          <label>
+            Direction:
+            <select
+              value={Direction}
+              onChange={(e) => setDirection(e.target.value)}
+            >
+              <option value="asc">Ascendent</option>
+              <option value="desc">Descendent</option>
+            </select>
+          </label>
+        </form>
+      </div>
+      <ul className="ItemList">
+        {ItemListData.map((item) => (
+          <li key={item.sku}>
+            <Item
+              url={item.url}
+              alt={item.name}
+              name={item.name}
+              price={item.price}
+              src={item.base_image}
+              hover_image={item.hover_image}
+            />
+          </li>
+        ))}
+      </ul>
+      {PaginationBtn ? (
+        <div className="PaginationBtn">
+          <button
+            onClick={() => {
+              setScrollPage(ScrollPage + 1);
+            }}
+          >
+            +
+          </button>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
 
